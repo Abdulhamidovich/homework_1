ec252528c60fcf6b07b3c4d18fe2d9b4ed0bf40f
Shamil Malikov <sh.abdulhamidovich@gmail.com>
2026-04-03 15:37:03 +0300

    fix formatting and image naming errors

--- homework-9.js
+++ homework-9.js
@@ -6,8 +6,8 @@ emailForm.addEventListener('submit', (event) => {
     const form = event.target;
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
-    console.log(data)
-})
+    console.log(data);
+});
 
 const modalForm = document.getElementById("registration-form");
 const modalWindow = document.querySelector('.modal');
@@ -16,32 +16,31 @@ const regButton = document.querySelector('.registration-button');
 const closeButton = document.querySelector('.close-button');
 
 regButton.addEventListener('click', () => {
-    modalWindow.classList.add('active')
+    modalWindow.classList.add('active');
     document.body.classList.add('no-scroll');
 });
+
 closeButton.addEventListener('click', () => {
-    modalWindow.classList.remove('active')
+    modalWindow.classList.remove('active');
     document.body.classList.remove('no-scroll');
 });
 
 const userLastName = document.getElementById('user-last-name');
-const userName = document.getElementById('user-name')
-const userDateOfBirth = document.getElementById('user-date-of-birth')
-const userLogin = document.getElementById('user-login')
-const userPass = document.getElementById('user-pass')
-const userPassConfirm = document.getElementById('user-pass-confirm')
+const userName = document.getElementById('user-name');
+const userDateOfBirth = document.getElementById('user-date-of-birth');
+const userLogin = document.getElementById('user-login');
+const userPass = document.getElementById('user-pass');
+const userPassConfirm = document.getElementById('user-pass-confirm');
 
-modalForm.addEventListener ('submit', (event) => {
-    if(userPass.value !=userPassConfirm.value) {
+modalForm.addEventListener('submit',(event) => {
+    if(userPass.value != userPassConfirm.value) {
         event.preventDefault();
         alert('Пароли не совпадают!');}
     else if(!modalForm.reportValidity()) {
         alert('Заполните все поля!')
-        event.preventDefault()
     }
     else {
         alert('Регистрация успешна!');
-        event.preventDefault();
         const form = event.target;
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
